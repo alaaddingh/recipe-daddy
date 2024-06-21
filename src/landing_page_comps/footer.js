92c89ae682b0d9ceaa97442e87593a6d48acb172
Carlos <carlosfvargas526@gmail.com>
2024-06-21 08:28:32 -0700

    various changes
    added fonts (need to connect them).
    styled the footer.
    added <header>.
    small other changes.

--- src/landing_page_comps/footer.js
+++ src/landing_page_comps/footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import "../index.css"
 
 function Footer() {
     return (
-        <footer>
-            <p> **footer goes here**</p>
-        </footer>
+        <nav>
+            <footer>
+                <img src="https://static.vecteezy.com/system/resources/previews/023/527/583/original/spoon-and-fork-black-icon-symbol-sign-design-transparent-background-free-png.png" width="80px" alt="logo" />
+                <a href='../about_us'>About Us</a>
+                <a href='../feedback'>Feedback</a>
+                <a href='https://github.com/alaaddingh/recipe-daddy' className='github'>
+                    <img src="../images/github-logo.png" width="10px" alt="github logo" />
+                    <p>GitHub</p>
+                </a>
+            </footer>
+        </nav>
     );
 }
 
