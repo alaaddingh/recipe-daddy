7dad731e27626acb7a78be8c60bf6d598041e549
Carlos <carlosfvargas526@gmail.com>
2024-06-21 11:02:14 -0700

    hamburger icon

--- src/landing_page_comps/header.js
+++ src/landing_page_comps/header.js
@@ -2,20 +2,31 @@ import React from 'react';
 import "../index.css"
 
 function Header() {
-return (
-    <nav>
-        <header>
-            <img src="https://static.vecteezy.com/system/resources/previews/023/527/583/original/spoon-and-fork-black-icon-symbol-sign-design-transparent-background-free-png.png" width="80px" alt="logo" />
-            <p className="nav--logotext">Recipe Diddy</p>
-            <div className="nav-icons">
-                {/* Replace **USER ICON** and **NAV ICON** placeholders with actual icons */}
-                <span className="user-icon">**USER ICON**</span>
-                <span className="nav-icon">**NAV ICON**</span>
-            </div>
-        </header>
-    </nav>
-);
+    return (
+        <nav>
+            <header>
+                <img src="https://static.vecteezy.com/system/resources/previews/023/527/583/original/spoon-and-fork-black-icon-symbol-sign-design-transparent-background-free-png.png" width="80px" alt="logo" />
+                <p className="nav--logotext">Recipe Diddy</p>
+                <div className="nav-icons">
+                    {/* Replace **USER ICON** and **NAV ICON** placeholders with actual icons */}
+                    <span className="user-icon">**USER ICON**</span>
+                    <Navbar />
+                </div>
+            </header>
+        </nav>
+    );
 }
+
+function Navbar() {
+    return (
+        <div className='hamburger-box'>
+            <div class='hamburger-line'></div>
+            <div class='hamburger-line'></div>
+            <div class='hamburger-line'></div>
+        </div>
+    );
+}
+
 
 
 export default Header;
