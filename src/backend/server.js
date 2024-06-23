9939b2e0ee745ac50c1f2fedca6ab0c07f1998ad
Carlos <carlosfvargas526@gmail.com>
2024-06-22 23:17:09 -0700

    small changes

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Enable CORS for all routes
 app.use(cors());
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 export const searchIngredients = async (query) => {
-    const response = await fetch(`http://localhost:5000/search-ingredients?query=${query}`);
+    const response = await fetch(`http://localhost:5001/search-ingredients?query=${query}`);
     const data = await response.json();
     return data;
 };
