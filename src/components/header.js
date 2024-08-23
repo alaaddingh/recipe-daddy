da4eafff57691b6a49a323987384416b1bfa634f
Carlos <carlosfvargas526@gmail.com>
2024-08-23 16:57:17 -0700

    logo

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,8 @@ function Header() {
     return (
         <nav>
             <header>
-                <img src="https://static.vecteezy.com/system/resources/previews/023/527/583/original/spoon-and-fork-black-icon-symbol-sign-design-transparent-background-free-png.png" width="80px" alt="logo" />
+                <p>logo</p>
+                {/* <img src="https://static.vecteezy.com/system/resources/previews/023/527/583/original/spoon-and-fork-black-icon-symbol-sign-design-transparent-background-free-png.png" width="80px" alt="logo" /> */}
                 <p className="nav--logotext">Recipe Daddy</p>
                 <div className="nav-icons">
                     {/* Replace **USER ICON** and **NAV ICON** placeholders with actual icons */}
